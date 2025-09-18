a5e1e8feb1e55950d718de46768cc8290692ec35
PavanNimkar <pavannnimkar@gmail.com>
2025-09-18 21:52:17 +0530

    gifs search added

--- src/layouts/search.jsx
+++ src/layouts/search.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/gif-context";
+import FilterGifs from "../components/FilterGifs";
+import Gif from "../components/Gif";
 
 export const Search = () => {
-  return <div>Search</div>;
+  const [searchResults, setSearchResults] = useState([]);
+  const { giphyResponse, filter } = GifState();
+  const { query } = useParams();
+
+  const fetchSearchResults = async () => {
+    const { data } = await giphyResponse.search(query, {
+      sort: "relevant",
+      lang: "en",
+      type: filter,
+      limit: 20,
+    });
+    console.log(data);
+
+    setSearchResults(data);
+  };
+
+  useEffect(() => {
+    fetchSearchResults();
+  }, [filter]);
+
+  return (
+    <div className="my-4">
+      <h2 className="text-5xl pb-3 font-extrabold">{query}</h2>
+      <FilterGifs alignLeft={true} />
+      {searchResults.length > 0 ? (
+        <div className="columns-2 md:columns-3 lg:columns-4 xl:column-5 gap-2">
+          {searchResults.map((gif) => {
+            return <Gif gif={gif} key={gif.id} />;
+          })}
+        </div>
+      ) : (
+        <span>
+          No Gifs found for {query}. Try Searching for Stickers instead.
+        </span>
+      )}
+    </div>
+  );
 };
